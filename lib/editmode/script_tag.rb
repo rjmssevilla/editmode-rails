b1d755bc3894370d709dadb67156b583c800fd7b
Xavi Ablaza <xlablaza@gmail.com>
2020-12-03 14:51:08 -0800

    Use before action with project id

--- lib/editmode/script_tag.rb
+++ lib/editmode/script_tag.rb
@@ -1,13 +1,12 @@
 module Editmode
-
   class ScriptTag
 
     def self.generate(*args)
       new(*args).output
     end
 
-    def initialize(options = {})
-      
+    def initialize(project_id=Editmode.project_id)
+      @project_id = project_id
     end
 
     def valid?
@@ -15,21 +14,17 @@ module Editmode
     end
 
     def output
-      
       str = <<-EDITMODE_SCRIPT
-  <script>window.chunksProjectIdentifier = '#{Editmode.project_id}'</script>
+  <script>window.chunksProjectIdentifier = '#{@project_id}'</script>
   <script>window.editmodeENV = '#{ENV["EDITMODE_ENV"] || 'production'}'</script>
   <script src="#{script_url}" async ></script>
       EDITMODE_SCRIPT
 
       str.respond_to?(:html_safe) ? str.html_safe : str
-
     end
 
-    def script_url 
+    def script_url
       ENV["EDITMODE_OVERRIDE_SCRIPT_URL"] || "https://static.editmode.com/editmode@2.0.0/dist/editmode.js"
     end
-
   end
-
 end
